e081a8dd51cac151f86808877938e7586f53a2dd
diogocsgomes <54036658+diogocsgomes@users.noreply.github.com>
2024-09-25 11:04:16 +0100

    Delete LetterSoup directory

--- LetterSoup/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Controls } from "./controls.component";
-export { default as GamePanel } from "./gamepanel.component";
-export { default as Title } from "./title.component";
-export { default as WordPanel } from "./wordpanel.component";
-export { default as Footer } from "./footer.component";
-export { default as PopUpWindow } from "./popupwindow.component";
